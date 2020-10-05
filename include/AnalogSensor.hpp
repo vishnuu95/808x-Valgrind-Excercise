810421d0591aa78fd014c82473754460d3a5e603
vishnuu95 <vishnuu@terpmail.umd.edu>
2020-10-05 16:49:00 -0400

    Added comments and added keyword explicit for single argument constructor.

--- include/AnalogSensor.hpp
+++ include/AnalogSensor.hpp
@@ -1,13 +1,34 @@
+/** Copyright 2020 Vishnuu A D
+ *  @file AnalogSensor.hpp which has the definition for the AnalogSensor class.
+*/
 #pragma once
 
 #include <iostream>
 
-class AnalogSensor
-{
-public:
-    AnalogSensor(unsigned int samples);
+/**
+ * @brief Class definition 
+*/
+class AnalogSensor {
+ public:
+    /**
+     * @brief Constructor
+     * @param samples unsigned int which stores number of samples to be read.
+     * @return None
+    */
+    explicit AnalogSensor(unsigned int samples);
+    /**
+     * @brief Destructor
+     * @param None
+     * @return None
+    */
     ~AnalogSensor();
+    /**
+     * @brief Read method of the class
+     * @param None
+     * @return int value
+    */
     int Read();
-private:
+
+ private:
     unsigned int mSamples;
 };
