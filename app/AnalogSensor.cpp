30df604aa4e20581b03c466ecaeff7a7eb5829c6
vishnuu95 <vishnuu@terpmail.umd.edu>
2020-10-05 16:48:22 -0400

    Added comments in AnalogSensor.cpp according to google style

--- app/AnalogSensor.cpp
+++ app/AnalogSensor.cpp
@@ -1,23 +1,37 @@
+/** Copyright 2020 Vishnuu A D 
+ * @file AnalogSensor.cpp
+ * @brief Implementation of AnalogSensor Class attributes and methods.
+ * 
+*/
 #include <AnalogSensor.hpp>
 #include <numeric>
 #include <vector>
 
+/** 
+ * @brief Class constructor.
+ * @param samples is an unsigned integer.
+ * @return None
+*/
 AnalogSensor::AnalogSensor(unsigned int samples)
-    : mSamples(samples)
-{
+    : mSamples(samples) {
 }
 
-AnalogSensor::~AnalogSensor()
-{
+/** @brief Destructor for the AnalaogSensor Class
+ * @param None
+ * @return None
+*/
+AnalogSensor::~AnalogSensor() {
 }
 
-int AnalogSensor::Read()
-{
+/** @brief Read method for the analog sensor class
+ * @param None
+ * @return result - int value of the result being returned.
+*/
+int AnalogSensor::Read() {
     std::vector<int> *readings = new std::vector<int>(mSamples, 10);
 
-    double result = std::accumulate( readings->begin(), readings->end(), 0.0 ) / readings->size();
+    double result = std::accumulate(readings->begin(), readings->end(),
+                 0.0) / readings->size();
     delete readings;
     return result;
 }
-
-
