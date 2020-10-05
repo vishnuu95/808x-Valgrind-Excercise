da0749e36d1d8a15049e693c05199ab7f1cfe974
vishnuu95 <vishnuu@terpmail.umd.edu>
2020-10-05 16:47:57 -0400

    Added comments in main according to google style.

--- app/main.cpp
+++ app/main.cpp
@@ -1,13 +1,20 @@
+/** Copyright 2020 Vishnuu A D 
+ * @file main.cpp is the main file that has a demo of the Analog Sensor Class working.
+*/
+
 #include <iostream>
 #include <AnalogSensor.hpp>
 
-int main()
-{
+/** 
+ * @brief main function to demonstrate working of AnalogSensor Class
+ * @param None
+ * @return None
+*/
+int main() {
     AnalogSensor lightSensor(5);
     std::cout << "Averaged sensor reading: " << lightSensor.Read() << std::endl;
     bool terminator = true;
-    if( terminator )
-    {
+    if (terminator) {
         std::cout << "DONE" << std::endl;
     }
     return 0;
